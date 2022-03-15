de3a5ecf48bbee48a7b10c7fa9c9ac50b030f7da
andradeviniicius <viniciusdandrade01@gmail.com>
2022-03-15 14:49:38 -0300

    btn close handler & style change

--- main.js
+++ main.js
@@ -1,39 +1,32 @@
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "Header")) {
-    // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
@@ -57,20 +50,14 @@ function windowHandler(windowId) {
   const currentWindow = document.getElementById(windowId)
   currentWindow.classList.add('window-opened')
   
-  
-  
-  
   dragElement(currentWindow);
 }
 
-const btnClose = document.querySelectorAll(".close")
-
-btnClose[0].addEventListener('click',()=>{
+function closeHandler(windowId) {
+  const currentWindow = document.getElementById(windowId)
   currentWindow.classList.remove('window-opened')
-})
-btnClose[1].addEventListener('click',()=>{
-  currentWindow.classList.remove('window-opened')
-})
+  
+}
 
 function maximizeHandler(windowId) {
 
